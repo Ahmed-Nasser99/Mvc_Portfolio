c591b00171d8d7ce7ed1076e2ae403a546957fd0
a.nasserr1 <a.nasserr1@outlook.com>
2024-02-09 16:45:27 +0200

    Add project files.

---
+++ Core/Models/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Models
+{
+    public class Address : BaseEntity
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ Core/Models/Owner.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Models
+{
+    public class Owner : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+        public string Profil { get; set; }
+        public Address Address { get; set; }
+    }
+}
